edf72018eaa0af68a3acd998865142bb95edc775
Nikhil Sunder <nsunder724@gmail.com>
2025-10-28 00:08:08 -0400

    minor fix
    Signed-off-by: Nikhil Sunder <nsunder724@gmail.com>

--- src/linear_environment/__about__.py
+++ src/linear_environment/__about__.py
@@ -1,5 +1,9 @@
+"""
+Metadata about the linear_environment package.
+"""
+
 __title__ = "linear_environment"
 __description__ = "A package for solving linear economic environments using SVAR models."
 __version__ = "0.0.1"
 __author__ = "Nikhil Sunder"
-__url__ = "https://github.com/erskordi/Autonnomous_Fed"
+__url__ = "https://github.com/erskordi/Autonomous_Fed"
